fa6565695efda43102b02fd431cd99b845de8ad7
Tymek <tymek_uramek@wp.pl>
2025-03-09 22:53:59 +0100

    First commit - stan projektu przed przeniesieniem

--- src/Staking.sol
+++ src/Staking.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
+<<<<<<< HEAD
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 
@@ -39,6 +40,7 @@ contract StakingDapp is ReentrancyGuard {
         _;
     }
 
+<<<<<<< HEAD
     constructor() payable {
         owner = msg.sender;
     }
@@ -128,3 +130,62 @@ contract StakingDapp is ReentrancyGuard {
     
 }
 
+=======
+    function pauseStaking() public {
+        paused = !paused; 
+    }
+
+
+    function stake() public payable notPaused hasEnoughETH(MIN_VALUE) {
+        require(msg.value >= MIN_VALUE,"Not enough ETH");
+        s_stakes[msg.sender] += msg.value;
+        s_stakesTimeStamps[msg.sender] = block.timestamp;
+    }
+
+    function unstake() public hasStaked(){
+        uint256 stakedAmount = s_stakes[msg.sender];
+        require(stakedAmount > 0, "No funds to unstake");
+        uint256 stakingDuration = block.timestamp - s_stakesTimeStamps[msg.sender];
+        require(stakingDuration >= MIN_STAKE_TIME, "Cannot unstake before 30 days");
+        
+        uint256 reward = calculateReward(stakedAmount, stakingDuration);
+         if(stakingDuration < MIN_STAKE_TIME){
+            uint256 penalty = (reward * EARLY_WITHDRAW_PENALTY) / 100;
+            reward -= penalty;
+            
+        }
+
+
+        uint256 totalAmount = stakedAmount + reward;
+        s_stakes[msg.sender] = 0;
+
+        payable(msg.sender).transfer(totalAmount);
+        
+        s_stakesTimeStamps[msg.sender] = 0;
+    }
+
+   function calculateReward(uint256 _amount, uint256 _duration) internal pure returns (uint256) {
+    uint256 yearlyReward;
+    
+    if (_duration <= 30 days) {
+                yearlyReward = (_amount * 2) / 100; // 5% rocznie
+            } else if (_duration <= 90 days) {
+                yearlyReward = (_amount * 5) / 100; // 7% rocznie
+            } else if (_duration <= 180 days) {
+                yearlyReward = (_amount * 10) / 100; // 10% rocznie
+            } else if (_duration <= 365 days) {
+                yearlyReward = (_amount * 12) / 100; // 12% rocznie
+            } else if(_duration <= 730 days){
+                yearlyReward = (_amount * 15) / 100; // 15% rocznie
+            }else{
+                yearlyReward = (_amount * 20) /100;
+            }
+            uint256 reward = (yearlyReward * _duration) / (365 days);
+        return reward;
+    }
+
+
+}
+
+
+>>>>>>> 7a2c402 (First commit - stan projektu przed przeniesieniem)
