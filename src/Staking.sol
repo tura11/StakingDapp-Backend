87f711984082dfbafb86e93f1d21595f0c211dc7
Tymek <tymek_uramek@wp.pl>
2025-03-12 21:44:17 +0100

    Poprawka kontraktu

--- src/Staking.sol
+++ src/Staking.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
-<<<<<<< HEAD
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-
 
 contract StakingDapp is ReentrancyGuard {
     address public owner;
@@ -40,7 +38,6 @@ contract StakingDapp is ReentrancyGuard {
         _;
     }
 
-<<<<<<< HEAD
     constructor() payable {
         owner = msg.sender;
     }
@@ -75,7 +72,7 @@ contract StakingDapp is ReentrancyGuard {
         return calculateReward(stakedAmount, stakingDuration);
     }
 
-    function unstake() public hasStaked nonReentrant  {
+    function unstake() public hasStaked nonReentrant {
         require(block.timestamp >= s_stakesTimeStamps[msg.sender] + minStakeTime, "Staking duration is too short");
         uint256 stakedAmount = s_stakes[msg.sender];
         uint256 stakingDuration = block.timestamp - s_stakesTimeStamps[msg.sender];
@@ -92,100 +89,47 @@ contract StakingDapp is ReentrancyGuard {
 
         require(address(this).balance >= totalAmount, "Not enough contract balance");
         payable(msg.sender).transfer(totalAmount);
-        
+
         emit Unstaked(msg.sender, stakedAmount, reward);
     }
 
     function calculateReward(uint256 _amount, uint256 _duration) public pure returns (uint256) {
         uint256 yearlyReward;
+
         if (_duration <= 30 days) {
-            yearlyReward = (_amount * 2) / 100;
+            yearlyReward = (_amount * 2) / 100; // 2% rocznie
         } else if (_duration <= 90 days) {
-            yearlyReward = (_amount * 5) / 100;
+            yearlyReward = (_amount * 5) / 100; // 5% rocznie
+        } else if (_duration <= 180 days) {
+            yearlyReward = (_amount * 10) / 100; // 10% rocznie
+        } else if (_duration <= 365 days) {
+            yearlyReward = (_amount * 12) / 100; // 12% rocznie
+        } else if (_duration <= 730 days) {
+            yearlyReward = (_amount * 15) / 100; // 15% rocznie
         } else {
-            yearlyReward = (_amount * 10) / 100;
+            yearlyReward = (_amount * 20) / 100; // 20% rocznie
         }
+
         uint256 reward = (yearlyReward * _duration) / 365 days;
         return reward;
     }
+
     function setMinStakeTime(uint256 _time) public onlyOwner {
-    require(_time >= 7 days, "Minimum stake time too low");
-    minStakeTime = _time;
+        require(_time >= 7 days, "Minimum stake time too low");
+        minStakeTime = _time;
     }
-    function userGetStakeInfo(address _user)external view returns(uint256 stakedAmount, uint256 reward, uint256 stakingTime) {
+
+    function userGetStakeInfo(address _user) external view returns(uint256 stakedAmount, uint256 reward, uint256 stakingTime) {
         return(s_stakes[_user], getReward(_user), s_stakesTimeStamps[_user]);
-        
     }
 
     function depositRewards() external payable onlyOwner {}
 
-
     function getOwner() external view returns (address) {
         return owner;
     }
+
     function getContractBalance() public view returns (uint256) {
-    return address(this).balance;
+        return address(this).balance;
     }
-    
-    
 }
-
-=======
-    function pauseStaking() public {
-        paused = !paused; 
-    }
-
-
-    function stake() public payable notPaused hasEnoughETH(MIN_VALUE) {
-        require(msg.value >= MIN_VALUE,"Not enough ETH");
-        s_stakes[msg.sender] += msg.value;
-        s_stakesTimeStamps[msg.sender] = block.timestamp;
-    }
-
-    function unstake() public hasStaked(){
-        uint256 stakedAmount = s_stakes[msg.sender];
-        require(stakedAmount > 0, "No funds to unstake");
-        uint256 stakingDuration = block.timestamp - s_stakesTimeStamps[msg.sender];
-        require(stakingDuration >= MIN_STAKE_TIME, "Cannot unstake before 30 days");
-        
-        uint256 reward = calculateReward(stakedAmount, stakingDuration);
-         if(stakingDuration < MIN_STAKE_TIME){
-            uint256 penalty = (reward * EARLY_WITHDRAW_PENALTY) / 100;
-            reward -= penalty;
-            
-        }
-
-
-        uint256 totalAmount = stakedAmount + reward;
-        s_stakes[msg.sender] = 0;
-
-        payable(msg.sender).transfer(totalAmount);
-        
-        s_stakesTimeStamps[msg.sender] = 0;
-    }
-
-   function calculateReward(uint256 _amount, uint256 _duration) internal pure returns (uint256) {
-    uint256 yearlyReward;
-    
-    if (_duration <= 30 days) {
-                yearlyReward = (_amount * 2) / 100; // 5% rocznie
-            } else if (_duration <= 90 days) {
-                yearlyReward = (_amount * 5) / 100; // 7% rocznie
-            } else if (_duration <= 180 days) {
-                yearlyReward = (_amount * 10) / 100; // 10% rocznie
-            } else if (_duration <= 365 days) {
-                yearlyReward = (_amount * 12) / 100; // 12% rocznie
-            } else if(_duration <= 730 days){
-                yearlyReward = (_amount * 15) / 100; // 15% rocznie
-            }else{
-                yearlyReward = (_amount * 20) /100;
-            }
-            uint256 reward = (yearlyReward * _duration) / (365 days);
-        return reward;
-    }
-
-
-}
-
-
->>>>>>> 7a2c402 (First commit - stan projektu przed przeniesieniem)
